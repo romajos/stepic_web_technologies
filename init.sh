8ab5132cfda84a122a3e636f318db2186d0bec20
romajos <unicorn.krimp@gmail.com>
2016-11-17 02:08:00 +0300

    Add removing defaul nginx configuration to init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln - s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
